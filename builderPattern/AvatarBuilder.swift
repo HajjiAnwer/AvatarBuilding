24154051f8425da0d65a97c6956ebca115878f77
Hajji Anwer <hanwer@tryspare.com>
2020-04-08 22:38:52 +0100

    first commit

---
+++ builderPattern/AvatarBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  AvatarBuilder.swift
+//  builderPattern
+//
+//  Created by Hajji Anwer on 4/8/20.
+//  Copyright © 2020 Hajji Anwer. All rights reserved.
+//
+
+import Foundation
---
+++ builderPattern/AvatarProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  AvatarProtocol.swift
+//  builderPattern
+//
+//  Created by Hajji Anwer on 4/8/20.
+//  Copyright © 2020 Hajji Anwer. All rights reserved.
+//
+
+import Foundation
